6e7c9c7f7d375b06a2e94db08e09c6b6593ec679
sdhaduk <sagardhaduk1@gmail.com>
2024-01-09 17:27:13 -0600

    added a trigger that creates a document of user data when a user signs up

---
+++ yt-api/functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+
+initializeApp();
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoURL: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+});
