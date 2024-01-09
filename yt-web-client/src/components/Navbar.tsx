fbdbc37b9c69c878531c896396c3e57b52a0c771
sdhaduk <sagardhaduk1@gmail.com>
2024-01-09 01:46:27 -0600

    created navbar

---
+++ yt-web-client/src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SignIn from "./SignIn";
+import { onAuthStateChangedHelper } from "@/utils/firebase";
+import { User } from "@firebase/auth";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return (
+    <nav className="flex justify-between items-center p-4">
+      <Link href="/">
+        <Image
+          src="/youtube-logo.svg"
+          alt="Youtube Logo"
+          width={90}
+          height={20}
+        />
+      </Link>
+      <SignIn user={user} />
+    </nav>
+  );
+};
+
+export default Navbar;
