9922acc8ed369d39a036de50adfea884aea16afb
sdhaduk <sagardhaduk1@gmail.com>
2024-01-09 18:46:22 -0600

    added a function to create a signed url for a user trying to upload a video

--- yt-api/functions/src/index.ts
+++ yt-api/functions/src/index.ts
@@ -2,9 +2,14 @@ import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import {initializeApp} from "firebase-admin/app";
 import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 initializeApp();
 const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "sagar-yt-skeleton-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -16,3 +21,29 @@ export const createUser = functions.auth.user().onCreate((user) => {
   firestore.collection("users").doc(user.uid).set(userInfo);
   logger.info(`User Created: ${JSON.stringify(userInfo)}`);
 });
+
+export const generateUploadUrl = onCall(
+  {maxInstances: 1},
+  async (request) => {
+    if (!request.auth) {
+      throw new functions.https.HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated"
+      );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000,
+    });
+
+    return {url, fileName};
+  }
+);
