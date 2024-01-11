aea0153c005139961c3d42edbe7452db51b51fc0
sdhaduk <sagardhaduk1@gmail.com>
2024-01-10 20:52:21 -0600

    added Video type, moved getFunctions to firebase.ts, and added getVideos function

--- yt-web-client/src/utils/functions.ts
+++ yt-web-client/src/utils/functions.ts
@@ -1,8 +1,18 @@
-import { getFunctions, httpsCallable } from "firebase/functions";
-
-const functions = getFunctions();
+import { httpsCallable } from "firebase/functions";
+import { functions } from "./firebase";
 
 const generateUploadUrl = httpsCallable(functions, "generateUploadUrl");
+const getVideosFunction = httpsCallable(functions, "getVideos");
+
+export interface Video {
+  id?: string;
+  uid?: string;
+  filename?: string;
+  status?: "processing" | "processed";
+  title?: string;
+  description?: string;
+  date?: Date;
+}
 
 export const uploadVideo = async (file: File) => {
   const response: any = await generateUploadUrl({
@@ -18,4 +28,10 @@ export const uploadVideo = async (file: File) => {
   });
 
   return;
-};  
+};
+
+export const getVideos = async () => {
+  const response = await getVideosFunction();
+
+  return response.data as Video[];
+};
