649e1730197e129d97b856d656eae36af4ea0690
sdhaduk <sagardhaduk1@gmail.com>
2024-01-10 20:51:36 -0600

    added getFunctions

--- yt-web-client/src/utils/firebase.ts
+++ yt-web-client/src/utils/firebase.ts
@@ -7,6 +7,7 @@ import {
   onAuthStateChanged,
   User,
 } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,6 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+export const functions = getFunctions();
 
 export const signInWithGoogle = () => {
   return signInWithPopup(auth, new GoogleAuthProvider());
